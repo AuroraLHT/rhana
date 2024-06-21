1432b0772460c14958402ceeb0289c0a56974e0a
AuroraLHT <auroralht@gmail.com>
2024-06-21 12:55:58 -0400

    add tool for fixing tilted rheed pattern

--- rhana/tools/restoration.py
+++ rhana/tools/restoration.py
@@ -1,12 +1,19 @@
 import numpy as np
 
-def suggest_restoration(rd, rmin=3):
-    rd.get_blobs()
-    db, db_i = rd.get_direct_beam()
+def suggest_restoration(rd, rdinst=None, rmin=3):
+    if rdinst is None:
+        rd.get_blobs()
+        db, db_i = rd.get_direct_beam(rmin=rmin)
+        laue_xy, laue_r = rd.laue_circle_analyse()
+        ss = rd.ss
+
+    else:
+        db, db_i, _ = rdinst.get_direct_beam(method="top", tracker=None, track=None, direct_beam_label=3)
+        laue_xy, laue_r = rdinst.laue_circle_analyse()
+        ss = rdinst.ss
+
     # ss, ss_i = rd.get_specular_spot()
     # laue_xy, laue_r = rd.get_0laue()
-    laue_xy, laue_r = rd.laue_circle_analyse()
-    ss = rd.ss
     laue_vector = np.array(db) - np.array(ss)
 
     angle =  np.rad2deg(
